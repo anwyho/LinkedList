460d6ddfb2e2d1907b76e5d5c25aa4836994cbd3
Anthony Ho <aawho@ucdavis.edu>
2018-07-24 09:37:52 -0700

    Commented out ArrayList import for commented test

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;  // for testCreateNodeWithInvalidType
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
